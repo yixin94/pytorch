d144b74719f9ab235860a0b4f2af02758ec9ed69
lixinyu <lixinyu@devgpu175.prn2.facebook.com>
2020-12-08 21:54:42 -0800

    webdataset prototype - LoadFilesFromDiskIterableDataset
    ghstack-source-id: d1690b95215a12d0aabede5c86cc5eb3745123c9
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/48955

--- torch/utils/data/datasets/common.py
+++ torch/utils/data/datasets/common.py
@@ -2,6 +2,41 @@ import os
 import fnmatch
 import warnings
 from typing import List, Union, Iterable
+from io import BufferedIOBase
+
+class StreamWrapper:
+    # this is a wrapper class which wraps streaming handle
+    def __init__(self, stream):
+        # if input is a StreamWrapper already, then transfer ownership
+        if isinstance(stream, StreamWrapper):
+            self.stream = stream()
+            stream.reset()
+        # only accept streaming obj
+        elif isinstance(stream, BufferedIOBase):
+            self.stream = stream
+        else:
+            warnings.warn("StreamWrapper can only wrap BufferedIOBase based obj, but got {}".format(type(stream)))
+            raise TypeError
+
+    def reset(self):
+        # behavior is undefined if calling any method other than close() after reset() is called
+        self.stream = None
+
+    def read(self, *args, **kw):
+        # put type ignore here to avoid mypy complaining too many args
+        res = self.stream.read(*args, **kw)  # type: ignore
+        return res
+
+    def close(self):
+        if self.stream:
+            self.stream.close()
+
+    def __del__(self):
+        self.close()
+
+    def __call__(self):
+        return self.stream
+
 
 def match_masks(name : str, masks : Union[str, List[str]]) -> bool:
     # empty mask matches any input name
@@ -36,3 +71,17 @@ def get_file_pathnames_from_root(
                 yield os.path.join(path, f)
         if not recursive:
             break
+
+
+def get_file_binaries_from_pathnames(pathnames : Iterable):
+
+    if not isinstance(pathnames, Iterable):
+        warnings.warn("get_file_binaries_from_pathnames needs the input be an Iterable")
+        raise TypeError
+
+    for pathname in pathnames:
+        if not isinstance(pathname, str):
+            warnings.warn("file pathname must be string type, but got {}".format(type(pathname)))
+            raise TypeError
+
+        yield (pathname, StreamWrapper(open(pathname, 'rb')))
